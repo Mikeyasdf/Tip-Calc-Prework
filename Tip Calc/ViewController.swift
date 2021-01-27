613606c022ecd2b9b029650037b0e7f104fbfe9e
Miguel <miguelespitia094@gmail.com>
2021-01-26 18:03:55 -0800

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Miguel Angel Espitia on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
